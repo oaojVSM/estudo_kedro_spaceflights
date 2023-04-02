06a157de178eb63b91307149f2274b28b73b7ed2
oaojVSM <jvsenedesi@gmail.com>
2023-04-02 22:41:52 +0000

    comentarios

--- src/estudo_kedro_spaceflights/pipeline_registry.py
+++ src/estudo_kedro_spaceflights/pipeline_registry.py
@@ -11,6 +11,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
+    pipelines = find_pipelines() # Este comando faz com que os pipelines criados nas pastas pipelines sejam automaticamente adicionados ao pipeline registry
+    pipelines["__default__"] = sum(pipelines.values()) # Este cria um pipeline __default__ que será rodado toda vez que o 'kedro run' for chamado sem mais nenhum argumento
     return pipelines
